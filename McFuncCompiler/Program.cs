40709f09afd0a3526d8bf5dc849fbc3cd31318c3
Mitchfizz05 <me@mitchfizz05.net>
2019-06-09 12:22:36 +0930

    Added parse and compile time stats to console output.

--- McFuncCompiler/Program.cs
+++ McFuncCompiler/Program.cs
@@ -12,10 +12,20 @@ namespace McFuncCompiler
             env.OutputPath = "../../TestData/datapack_compiled";
             env.Constants.Add("mcfunc_compiler_version", Version.ToString(2));
 
+            DateTime startParse = DateTime.UtcNow;
+
             var parser = new Parser.Parser(env);
             McFunction mcFunction = parser.Parse("test_data:test");
 
+            TimeSpan parseTime = DateTime.UtcNow - startParse;
+            Logger.Info($"Parsed in {parseTime.Milliseconds}ms.");
+
+            DateTime startCompile = DateTime.UtcNow;
+
             mcFunction.Compile(env);
+
+            TimeSpan compileTime = DateTime.UtcNow - startCompile;
+            Logger.Info($"Compiled in {compileTime.Milliseconds}ms.");
 
             mcFunction.Save(env);
 
